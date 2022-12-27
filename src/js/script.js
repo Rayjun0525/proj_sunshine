31ff7781c09ec1f50afe173876a1527d60e84625
Rayjun0525 <rayjun0525@gmail.com>
2022-12-27 15:39:57 +0900

    c

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+const text = document.getElementById("text");
+let i = 0;
+const message = "Project Sunshine";
+const typing = () => {
+  if (i < message.length) {
+    text.innerHTML += message.charAt(i);
+    i++;
+    setTimeout(typing, 50);
+  }
+};
+typing();
